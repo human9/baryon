d96da78c42aa6bfaf3401a8be5853198043bdb55
John Salamon <salamonj9@gmail.com>
2017-01-16 03:13:25 +1030

    window

--- src/base/window.rs
+++ src/base/window.rs
@@ -5,17 +5,49 @@ extern crate libc;
 use core::system;
 use core::message;
 
+use std::time::Duration;
+use std::thread;
+
 pub struct Windowing {
     name: &'static str,
     status: system::Status,
+    window: glutin::Window,
 }
 
 impl system::System for Windowing {
     fn init() -> Self {
-        Windowing { name: "Windowing", status: system::Status::Okay }
+
+        let window = glutin::WindowBuilder::new()
+            .with_title("baryon")
+            .build()
+            .unwrap();
+
+        unsafe { 
+            
+            window.make_current();
+
+            gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+            gl::ClearColor(0.0, 0.0, 0.0, 0.0);
+        
+        };
+
+        Windowing { name: "Windowing", status: system::Status::Okay, window: window}
     }
 
     fn run(&self) -> system::Status {
+        for event in self.window.poll_events() {
+            //unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+
+            match event {
+                glutin::Event::Closed => break,
+                _ => ()
+            }
+        }
+        self.window.swap_buffers();
+
+        thread::sleep(Duration::from_millis(10));
+
         self.status
     }
 
@@ -23,30 +55,3 @@ impl system::System for Windowing {
         self.name
     }
 }
-
-
-    /*
-    let window = glutin::WindowBuilder::new()
-        .with_title("baryon")
-        .build()
-        .unwrap();
-
-    unsafe { window.make_current() };
-    
-    unsafe {
-        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-        gl::ClearColor(0.0, 0.0, 0.0, 0.0);
-    }
-
-    for event in window.poll_events() {
-        //unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
-
-        match event {
-            glutin::Event::Closed => break,
-            _ => ()
-        }
-    }
-    window.swap_buffers();
-*/
-
